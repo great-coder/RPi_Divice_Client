d075563d825a47b45c625881837b828047e3a6c2
Mohammad-Dehghan <badil2013@hotmail.com>
2018-01-30 16:40:54 +0330

    Use security validation middleware

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+__author__ = 'Mohammad Dehghan'
+
 import sys
 from services import net
 from controllers.account_controller import login
+from security import validation
 
 
 def main():
     try:
         net.connectivity(10, 5)
-        login()
+        # Check if there is any token from before and check it's expiration, then do login
+        if not validation.token_validation():
+            login()
+        else:
+            # TODO: Connect to Chat Hub and broadcast a Hello message! Then go to idle state!!!
+            print("Ex-provided token is still valid, use that.")
     except:
         error = sys.exc_info()[0]
         print("Exception occurred!")
