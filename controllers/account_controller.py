3210195c587d1a34f7fd4336b72cc7023ba033b3
Mohammad-Dehghan <badil2013@hotmail.com>
2018-01-30 23:35:12 +0330

    Manager method to abstract more from top module

--- controllers/account_controller.py
+++ controllers/account_controller.py
@@ -1,6 +1,22 @@
-from services.net import send_request
+__author__ = 'Mohammad Dehghan'
+
+from services import net
+from security import validation
+
+
+def manage():
+    # Check if there is any token from before and check it's expiration, then do login
+    if not validation.token_validation():
+        login()
+    else:
+        # TODO: Connect to Chat Hub and broadcast a Hello message! Then go to idle state!!!
+        print("Ex-provided token is still valid, use that.")
 
 
 def login():
+    # TODO: Make a request to HomeServer.API to login
     print("login")
-    send_request("http://localhost:51130", "/oauth", "POST", "grant_type=password&username=username&password=password")
+    # TODO: Read server(url,port),identity(username,password) and token from secrets.json through secrets.py
+    net.send_request("http://localhost:51130", "/oauth", "POST",
+                     "grant_type=password&username=username&password=password")
+    # secrets.write_token(data)
