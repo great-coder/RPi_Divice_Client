a47e4b69df281d7c6fd2a861396fd9587077fd72
Mohammad-Dehghan <badil2013@hotmail.com>
2018-01-30 23:41:36 +0330

    Move login responsibility to account_controller.manage()

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 __author__ = 'Mohammad Dehghan'
 
 import sys
-from services import net
-from controllers.account_controller import login
-from security import validation
+from services.net import connectivity
+from controllers.account_controller import manage
 
 
 def main():
     try:
-        net.connectivity(10, 5)
-        # Check if there is any token from before and check it's expiration, then do login
-        if not validation.token_validation():
-            login()
-        else:
-            # TODO: Connect to Chat Hub and broadcast a Hello message! Then go to idle state!!!
-            print("Ex-provided token is still valid, use that.")
+        connectivity(20, 5)
+        manage()
     except:
         error = sys.exc_info()[0]
         print("Exception occurred!")
